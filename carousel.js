e10ffc79dd426c601ca220fba423d05804db30f8
Dominik Gubo <dominik.gubo2@gmail.com>
2022-08-04 01:34:54 +0200

    Added animation on doc and menu load

--- carousel.js
+++ carousel.js
@@ -2,7 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
   const config = {
     type: "carousel",
     perView: 3,
-    gap: 24
+    gap: 24,
+    focusAt: 'center',
+    breakpoints: {
+      640:{
+        perView: 3
+      },
+      1024:{
+        perView: 5
+      }
+    }
 }
 
 new Glide(".glide", config).mount();
--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const menu = document.querySelector(".menu");
 const allDivs = document.querySelectorAll("div:not(.menu)");
 console.log(allDivs)
 
+
 hamburgerButton.addEventListener('click', (e)=>{
     menu.classList.toggle("display");
+    menu.classList.toggle("animation-fade__slow");
     for(var i=0; i<allDivs.length; i++){
         allDivs[i].classList.toggle("displayOff");
     }
@@ -13,6 +15,7 @@ hamburgerButton.addEventListener('click', (e)=>{
 
 closeButton.addEventListener('click', (e)=>{
     menu.classList.toggle("display");
+    menu.classList.toggle("animation-fade__slow");
     for(var i=0; i<allDivs.length; i++){
         allDivs[i].classList.toggle("displayOff");
     }
